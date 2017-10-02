2fa02287aa2267c47f994fb399beb65408c4ce57
ManoharreddyN <manohar.reddynarapareddy@dal.ca>
2017-10-02 15:46:25 +0000

    task

--- task.py
+++ task.py
@@ -1,2 +1,10 @@
-def
-x
+import file1()
+from datetime import date,import timedelta
+
+def List_crimes(crimes):
+   incidents = make_request()
+  for i in i:
+  crimes.point(float(incident['Y']), float(incident['X']), 0, 0)
+ 
+
+
